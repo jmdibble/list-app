c9e8c764eab2435151ce79d5cc5373dd4b78b207
Joshua Dibble <jdibble@mymuse.uk>
2019-01-07 12:09:02 +0000

    Added more pages and continued cleaning code

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import "package:flutter/material.dart";
 import "package:flutter/rendering.dart";
 
 import "./pages/auth.dart";
+import "./pages/products_admin.dart";
+import "./pages/products.dart";
 
 void main() {
 //  debugPaintSizeEnabled = true;
@@ -16,12 +18,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
 //      debugShowMaterialGrid: true,
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          brightness: Brightness.light,
-          primarySwatch: Colors.deepPurple,
-          accentColor: Colors.deepOrange
-      ),
-      home: AuthPage(),
+      theme: ThemeData(brightness: Brightness.light, primarySwatch: Colors.deepPurple, accentColor: Colors.deepOrange),
+//      home: AuthPage(),
+      routes: {
+        "/": (BuildContext context) => ProductsPage(),
+        "/admin": (BuildContext context) => ProductsAdminPage(),
+      },
     );
   }
 }
--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -1,10 +1,54 @@
 import "package:flutter/material.dart";
 
+import "./products.dart";
+import "./product_create.dart";
+import "./product_list.dart";
+
 class ProductsAdminPage extends StatelessWidget {
-@override
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Product Manager"),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text("Choose"),
+              ),
+              ListTile(
+                title: Text("All Products"),
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (BuildContext context) => ProductsPage()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Text("Products Manager"),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.create),
+                text: "Create Product",
+              ),
+              Tab(
+                icon: Icon(Icons.list),
+                text: "My Products",
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(children: <Widget> [
+          ProductCreatePage(),
+          ProductListPage(),
+        ],
+        ),
       ),
     );
   }
